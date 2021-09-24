536196d16930441bdf5d4c006fa2063f7c142206
felixyau <yau.lokhang0530@gmail.com>
2021-09-25 02:08:04 +0800

    m

--- App.py
+++ App.py
@@ -7,5 +7,5 @@ app.register_blueprint(encryption, url_prefix="/")
 app.register_blueprint(index, url_prefix="/")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000) #debug true = change file will refresh the server
+    app.run(debug=True, port=8000) ##debug true = change file will refresh the server
 
