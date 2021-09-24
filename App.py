532b8c96c2350d689c357adf95ff65eccb30f233
felixyau <yau.lokhang0530@gmail.com>
2021-09-25 02:28:43 +0800

    dotenv

--- App.py
+++ App.py
@@ -1,11 +1,14 @@
+import os
+from dotenv import load_dotenv
 from routes.encryption import encryption
 from routes.index import index
 from flask import Flask
+
+load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(encryption, url_prefix="/")
 app.register_blueprint(index, url_prefix="/")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000) ##debug true = change file will refresh the server
-
+    app.run(debug=True, port=int(os.getenv("PORT")) or 8000) #debug true = change file will refresh the server
