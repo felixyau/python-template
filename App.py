e0f5aba5a8393a571af914bdb77f243736de87b9
felixyau <yau.lokhang0530@gmail.com>
2021-09-25 09:00:34 +0800

    m

--- App.py
+++ App.py
@@ -1,13 +1,17 @@
 from dotenv import load_dotenv
 from routes.encryption import encryption
-from routes.index import index
-from flask import Flask
+from flask import Flask, jsonify
 
 load_dotenv()
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    data = { "first": 1 }
+    return jsonify(data)
+
 app.register_blueprint(encryption, url_prefix="/")
-app.register_blueprint(index, url_prefix="/")
 
 if __name__ == "__main__":
     app.run() #debug true = change file will refresh the server
--- routes/index.py
+++
@@ -1,8 +0,0 @@
-from flask import request, jsonify, Blueprint 
-
-index = Blueprint("index", __name__)
-
-@index.route("/")
-def home():
-    data = { "first": 1 }
-    return jsonify(data)
