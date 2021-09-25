8393aaf66e24170b149f1bfa99ab415ce613d395
felixyau <yau.lokhang0530@gmail.com>
2021-09-25 15:39:52 +0800

    m

--- routes/tic_tac_toe.py
+++ routes/tic_tac_toe.py
@@ -6,23 +6,23 @@ tic_tac_toe = Blueprint("tic_tac_toe", __name__)
 
 # function name cant be same as blueprint
 
-
-@tic_tac_toe.route("/", methods=["POST"])
+@tic_tac_toe.route("/", methods=["GET", "POST"])
 def handle():
-    battleId = request.get_json()["battleId"]
-    url = 'https://cis2021-arena.herokuapp.com//tic-tac-toe/start/' + battleId
-    # init(url)
-    data = {
-        "action": "putSymbol",
-        "position": "SE"
-    }
-    messages = SSEClient(url)
-    print("msg", messages)
-    for msg in messages:
-        print(msg.data)
-        res = requests.post(url, json=data)
+    if request.method == "POST": 
+        battleId = request.get_json()["battleId"]
+        print("id:", battleId)
+        url = 'https://cis2021-arena.herokuapp.com//tic-tac-toe/start/' + battleId
+        # init(url)
+        data = {
+            "action": "putSymbol",
+            "position": "SE"
+        }
+        messages = SSEClient(url)
+        print("msg", messages)
+        for msg in messages:
+            print(msg.data)
+            res = requests.post(url, json=data)
     return "hello"
-
 
 def init(url):
     return requests.get(url).json()
