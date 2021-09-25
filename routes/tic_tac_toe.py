4b72330700b8275eca58b755aac2d206ba4307f7
felixyau <yau.lokhang0530@gmail.com>
2021-09-25 15:18:51 +0800

    m

--- routes/tic_tac_toe.py
+++ routes/tic_tac_toe.py
@@ -4,21 +4,25 @@ from sseclient import SSEClient
 
 tic_tac_toe = Blueprint("tic_tac_toe", __name__)
 
-#function name cant be same as blueprint
+# function name cant be same as blueprint
+
+
 @tic_tac_toe.route("/", methods=["POST"])
 def handle():
-    battleId=""
-    battleId = jsonify(request.get_json())["battleId"]
+    battleId = request.get_json()["battleId"]
     url = 'https://cis2021-arena.herokuapp.com//tic-tac-toe/start/' + battleId
-    init(url)
-    # url = "http://127.0.0.1:5000/hello/"
+    # init(url)
+    data = {
+        "action": "putSymbol",
+        "position": "SE"
+    }
     messages = SSEClient(url)
     print("msg", messages)
     for msg in messages:
         print(msg.data)
+        res = requests.post(url, json=data)
     return "hello"
-    
+
+
 def init(url):
     return requests.get(url).json()
-
-
